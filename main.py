246c1cb24c5a402deb49ad444dbb8eb9ef1f4386
Dubin4 <71787435+R1ccoon@users.noreply.github.com>
2023-02-10 18:20:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,9 @@ def main():
     if not toponym:
         print('No data')
     else:
-        # longitude, latitude = get_coordinates(toponym)
-        # ll_spn = f"ll={longitude},{latitude}&spn=0.005,0.005"
-        # show_map(ll_spn, "map")
 
         ll, spn = get_ll_span(toponym)
+
         ll_spn = f"ll={ll}&spn={spn}"
         point = f"pt={ll},pm2wtl"
         show_map(ll_spn, "map", point)
